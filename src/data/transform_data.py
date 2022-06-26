d219a027e93dbb57db5bcbb3b0d65103359b92eb
Luis Gabriel Diaz Fonseca <lgdiazf@unal.edu.co>
2022-06-26 16:32:23 +0000

    Funcion para transformar datos
    se leen los archivos .xlsx y .xls y se pasan a formato csv

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -1,3 +1,20 @@
+
+
+import pandas as pd
+import os
+
+cwd=os.getcwd()
+
+
+
+path_landing = os.path.join(cwd, 'data_lake/landing/') 
+path_raw = os.path.join(cwd, 'data_lake/raw/') 
+list_files = os.listdir(path_landing)
+
+
+
+
+
 def transform_data():
     """Transforme los archivos xls a csv.
 
@@ -7,7 +24,22 @@ def transform_data():
     H23.
 
     """
-    raise NotImplementedError("Implementar esta función")
+
+    for file in list_files:
+        nombre = file.split(".")[0] 
+        try :
+            df_energia = pd.read_excel(path_landing + file,engine='openpyxl')
+        except :
+            df_energia = pd.read_excel(path_landing + file )
+        df_energia_filtrado = df_energia.dropna(thresh=24)
+        if list(df_energia_filtrado.columns.values)[0].upper() != "FECHA":
+            df_energia_filtrado.to_csv(path_raw + nombre + '.csv',index=False,header=False)
+        else :
+            df_energia_filtrado.to_csv(path_raw + nombre + '.csv',index=False,header=True)
+      
+        
+
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
