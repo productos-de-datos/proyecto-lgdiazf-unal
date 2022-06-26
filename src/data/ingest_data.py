b41a3a0f1531543b774cbccbf856125361093554
Luis Gabriel Diaz Fonseca <lgdiazf@unal.edu.co>
2022-06-26 14:52:19 +0000

    Se implementa la funcion de ingest_data

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -6,15 +6,18 @@ Módulo de ingestión de datos.
 import json
 import requests
 import os
-cwd=os.getcwd()
+
 from pathlib import Path
 
 
 url_tree_repo = "https://api.github.com/repos/jdvelasq/datalabs/git/trees/master"
-
+url_path_raw = 'https://raw.githubusercontent.com/jdvelasq/datalabs/master/datasets/precio_bolsa_nacional/xls/'
+cwd=os.getcwd()
 
 
 data_tree_repo : json  = requests.get(url_tree_repo).json()['tree']
+
+
 
 def get_tree_sha(files:json,folder:str):
     path : str= ""
@@ -38,11 +41,6 @@ def guardar_data(url:str,path:str):
   resp = requests.get(url, allow_redirects=True)
   filename = Path(path)
   filename.write_bytes(resp.content)
-#   file = open(path, 'w')
-#   file.write(resp.content)
-#   file.close()
-
-
 
 def ingest_data():
     """Ingeste los datos externos a la capa landing del data lake.
@@ -52,9 +50,9 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    for i in  resp.json()['tree']:
-        url = 'https://raw.githubusercontent.com/jdvelasq/datalabs/master/datasets/precio_bolsa_nacional/xls/' + str(i["path"])
-        path = os.path.join(cwd,"data_lake/landing/"+str(i['path']))
+    for archivo in  resp.json()['tree']:
+        url = url_path_raw  + str(archivo["path"])
+        path = os.path.join(cwd,"data_lake/landing/" + str(archivo['path']))
         guardar_data(url,path)
     #raise NotImplementedError("Implementar esta función")
 
