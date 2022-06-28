60d423388b119d1c06e35e39365ab9f8fe206df0
Luis Gabriel Diaz Fonseca <lgdiazf@unal.edu.co>
2022-06-28 02:08:08 +0000

    funcion de compute_monthly

--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -1,3 +1,12 @@
+import pandas as pd
+
+import os
+
+cwd=os.getcwd()
+
+path_datos = os.path.join(cwd, 'data_lake/cleansed/precios-horarios.csv')
+path_datos_computados = os.path.join(cwd, 'data_lake/business/precios-mensuales.csv')
+
 def compute_monthly_prices():
     """Compute los precios promedios mensuales.
 
@@ -12,10 +21,20 @@ def compute_monthly_prices():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    df_datos = pd.read_csv(path_datos)
+    df_datos[["fecha"]] = df_datos[["fecha"]].apply(pd.to_datetime)
+    df_datos.index = df_datos["fecha"]
+    grupo_df_datos = df_datos.groupby(pd.Grouper(freq="M"))
+    df_consolidado = grupo_df_datos['precio'].mean()
+
+
+    df_consolidado.to_csv(path_datos_computados ,index=True,header=True)
+
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
+    compute_monthly_prices()
 
     doctest.testmod()
