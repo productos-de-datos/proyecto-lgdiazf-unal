189d8d89b8c0f1a0950867bcb15364d910112e6b
Luis Gabriel Diaz Fonseca <lgdiazf@unal.edu.co>
2022-06-30 12:08:50 +0000

    arreglo test monthly

--- src/data/compute_data_monthly_test.py
+++ src/data/compute_data_monthly_test.py
@@ -1,7 +1,7 @@
 from operator import imod
 import os
 import pandas as pd
-import compute_daily_prices
+import compute_monthly_prices
 
 cwd=os.getcwd()
 
@@ -10,7 +10,7 @@ cwd=os.getcwd()
 class TestIngrestData:
 
     def test_ingest_data(self):
-        compute_daily_prices.compute_daily_prices()
+        compute_monthly_prices.compute_monthly_prices()
         path = os.path.join(cwd, 'data_lake/business/precios-mensuales.csv')
         assert os.path.exists(path) is True
 
