3bd8584e867d0a188f050ac57e20026134b0c159
Luis Gabriel Diaz Fonseca <lgdiazf@unal.edu.co>
2022-07-04 13:30:56 +0000

    pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,125 +1,129 @@
-
-import sys
-import time
 import luigi
+import sys
+import os
+import pandas as pd
 
 sys.path.append("src/data")
-
 
 import ingest_data
 import transform_data
 import clean_data
 import compute_daily_prices
 import compute_monthly_prices
-    
 
-class IngestData(luigi.Task):
+
+url_path_raw = 'https://raw.githubusercontent.com/jdvelasq/datalabs/master/datasets/precio_bolsa_nacional/xls/'
+cwd=os.getcwd()
+
+class TaskA(luigi.Task):
+
+  def output(self):
+    return luigi.LocalTarget('TaskA.output')
+
+  def run(self):
+    lista = ingest_data.get_file_list()
+    with self.output().open('w') as f :
+      for archivo in lista:
+        url = url_path_raw  + str(archivo["path"])
+        path = os.path.join(cwd,"data_lake/landing/" + str(archivo['path']))
+        ingest_data.guardar_data(url,path)
+        f.write(path+"\n")
+
+
+
+class TaskB(luigi.Task):
 
     def output(self):
-        return luigi.LocalTarget('ingest.log')
+        return luigi.LocalTarget("TaskB.output")
+
+    def requires(self):
+        return TaskA()
 
     def run(self):
-        with self.output().open('w') as f:
-            ingest_data.ingest_data()
-            #ingest_data.ingest_data()
-            
-            
+      path_raw = os.path.join(cwd, 'data_lake/raw/') 
+      with self.input().open("r") as i:
+        lineas = i.readlines()
+        with self.output().open("w") as f:
+          for linea in lineas:
+            path = str(linea).replace("\n","").split("/")[-1]
+            df_energia = transform_data.load_df(path)
+            df_esquema = transform_data.set_esquema(df_energia)
+            df_transformado = transform_data.transform_df(df_esquema)
+            # df = transform_data.get_df(path)
+            path_csv = transform_data.guardar_df(path,path_raw,df_transformado)
+            f.write(path_csv+"\n")
 
-            f.write('ingest data')
+
+
+class TaskC(luigi.Task):
+
+  def arreglar_lista(self,linea):
+    return linea.replace("\n","")
+
+  def requires(self):
+    return TaskB()
+
+  def output(self):
+    return luigi.LocalTarget('TaskC.output')
+
+  def run(self):
+    with self.input().open("r") as i:
+      lineas = i.readlines()
+      lista_archivos = list(lineas)
+      lista_archivos = list(map(self.arreglar_lista,lista_archivos))
+      df_completo = clean_data.get_df_completo(lista_archivos)
+      df_formateado = clean_data.formatear_df(df_completo)
+      path = os.path.join(cwd,"data_lake/cleansed/" + "precios-horarios.csv" )
+      clean_data.guardar_df(path,df_formateado)
+      with self.output().open("w") as f:
+        f.write(path)
         
-class TransformData(luigi.Task):
+class TaskD(luigi.Task):
 
-    def __init__(self, *args, **kwargs):
-      super().__init__(*args, **kwargs)
-      transform_data.transform_data()
+  def requires(self):
+    return TaskC()
 
+  def output(self):
+    return luigi.LocalTarget('TaskD.output')
 
+  def run(self):
+    with self.input().open("r") as i:
+      path_data = str(i.read())
+      df_datos = pd.read_csv(path_data)
+      df_promedio_diario = compute_daily_prices.get_datos(df_datos) 
+      path_salida = os.path.join(cwd, 'data_lake/business/precios-diarios.csv')
+      compute_daily_prices.guardar_df(path_salida,df_promedio_diario)
+      with self.output().open("w") as f:
+        f.write(path_salida)        
 
-    def requires(self):
-        return IngestData()
+class TaskE(luigi.Task):
 
-    def output(self):
-        return luigi.LocalTarget('transform.log')
+  def requires(self):
+    return [TaskC(),TaskD()]
 
-    def run(self):
-        pass
-        
-        #with self.input().open('r') as i:
-        #     f = self.output().open('w')
-        #     b = transform_data.transform_data()
-        #     time.sleep(30)
-        #     f.write(str(b))   
-        #     f.close()         
+  def output(self):
+    return luigi.LocalTarget('TaskE.output')
 
-class CleanData(luigi.Task):
-
-    def requires(self):
-        return [IngestData(),TransformData()]
-
-    def output(self):
-        return luigi.LocalTarget('clean.log')
-
-    def run(self):
-        with self.output().open('w') as f:
-            
-            clean_data.clean_data()
-            f.write('clean data')
-
-class ComputeDailyPrices(luigi.Task):
+  def run(self):
+    with self.input()[0].open("r") as i:
+      path_data = str(i.read())
+      df_datos = pd.read_csv(path_data)
+      df_formateado =  compute_monthly_prices.format_df(df_datos)
+      path_datos_computados = os.path.join(cwd, 'data_lake/business/precios-mensuales.csv')
+      compute_monthly_prices.guardar_df(df_formateado,path_datos_computados)
+      with self.output().open("w") as f:
+        f.write(path_datos_computados)  
 
 
-    def requires(self):
-        return [IngestData(),TransformData(),CleanData()]
+def correr_pipeline():
+  luigi.run(["TaskE"],local_scheduler=True)
 
-    def output(self):
-        return luigi.LocalTarget('compute_daily.log')
-
-    def run(self):
-         with self.output().open('w') as f:
-            
-            compute_daily_prices.compute_daily_prices()
-            f.write('compute Daily')
-
-class ComputeMounthlyPrices(luigi.Task):
-
-
-    def requires(self):
-        return [ComputeDailyPrices()]
-
-    def output(self):
-        return luigi.LocalTarget('compute_mounthly.log')
-
-    def run(self):
-         with self.output().open('w') as f:
-            
-            compute_monthly_prices.compute_monthly_prices()
-            f.write('compute mounthly')
-
-"""
-Construya un pipeline de Luigi que:
-
-* Importe los datos xls
-* Transforme los datos xls a csv
-* Cree la tabla unica de precios horarios.
-* Calcule los precios promedios diarios
-* Calcule los precios promedios mensuales
-
-En luigi llame las funciones que ya creo.
-
-
-"""
-
-
-    # raise NotImplementedError("Implementar esta función")
-    #luigi.build([IngestData(),TransformData(),CleanData(),ComputeDailyPrices(),ComputeMounthlyPrices()], local_scheduler=True)
-    
 
 if __name__ == "__main__":
     import doctest
+    luigi.run(["TaskE"],local_scheduler=True)
 
     #luigi.build([IngestData(),TransformData(),CleanData(),ComputeDailyPrices(),ComputeMounthlyPrices()])
     #luigi.build([IngestData(),TransformData(),CleanData(),ComputeDailyPrices(),ComputeMounthlyPrices()])
 
-    luigi.run(["TransformData"])
-
-    doctest.testmod()
+    
