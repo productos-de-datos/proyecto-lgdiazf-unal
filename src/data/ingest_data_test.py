db97b42bce5eee9e8014872df6bbe4e8bd60503d
Luis Gabriel Diaz Fonseca <lgdiazf@unal.edu.co>
2022-07-04 05:47:18 +0000

    fin test ingest_data

--- src/data/ingest_data_test.py
+++ src/data/ingest_data_test.py
@@ -1,4 +1,6 @@
+
 import ingest_data
+import create_data_lake
 import os
 import esquema_data as esquema
 import pandas as pd
@@ -20,21 +22,29 @@ class TestIngrestData:
         assert os.path.exists(path_archivo) is True
         os.system("rm -rf .tmp")
 
+    def test_ingest_data(self):
 
+        os.system("rm -rf data_lake")
+        create_data_lake.create_data_lake()
+        ingest_data.ingest_data()
+        for path_archivos in esquema.get_esquema():
+            assert os.path.exists(path_archivos) is True
 
-    # def test_ingest_data(self):
-    #     ingest_data.ingest_data()
-    #     for path_archivos in esquema.get_esquema():
-    #         assert os.path.exists(path_archivos) is True
+        os.system("rm -rf data_lake")
 
-    # def test_cargar_pandas(self):
-    #     #ingest_data.ingest_data()
-    #     try:
-    #         path = os.path.join(cwd, esquema.get_esquema()[0])
-    #         df = pd.read_excel(path,engine='openpyxl')
-    #         print(df)
-    #     except:
-    #         assert False
+    def test_cargar_pandas(self):
+
+        os.system("rm -rf data_lake")
+        create_data_lake.create_data_lake()
+        ingest_data.ingest_data()
+        try:
+            path = os.path.join(cwd, esquema.get_esquema()[0])
+            df = pd.read_excel(path,engine='openpyxl')
+            print(df)
+        except:
+            assert False
+        finally :
+            os.system("rm -rf data_lake")
 
 
 
