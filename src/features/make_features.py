b04253cf07fc816a051cb18ad400aeae0cb0c61d
Luis Gabriel Diaz Fonseca <lgdiazf@unal.edu.co>
2022-07-05 06:40:57 +0000

    arreglo make_feature

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -1,14 +1,15 @@
 import os
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 cwd = os.getcwd()
 
 path_datos = os.path.join(cwd,"data_lake/business/precios-diarios.csv")
 path_salida = os.path.join(cwd,"data_lake/business/features/precios_diarios.csv")
-
+# coding=utf-8
 def make_features():
     """Prepara datos para pronóstico.
-
     Cree el archivo data_lake/business/features/precios-diarios.csv. Este
     archivo contiene la información para pronosticar los precios diarios de la
     electricidad con base en los precios de los días pasados. Las columnas
@@ -22,13 +23,30 @@ def make_features():
     """
 
     df = pd.read_csv(path_datos)
-    df =  df.iloc[13: , :]
+    # df =  df.iloc[13: , :]
 
-    df_variables_explicativas = get_df_variables_explicativas(df,p=13)
+    # df_variables_explicativas = get_df_variables_explicativas(df,p=13)
 
-    df_completo = df.join(df_variables_explicativas)
+    # df_completo = df.join(df_variables_explicativas)
 
+    # df_completo.to_csv(path_salida,index=False,header=True)
+
+    data = get_precios(df)
+    observed_scaled, X = get_precio_transformado(data)
+
+    df1 = pd.DataFrame(X)
+    df2 = pd.DataFrame(observed_scaled).rename(columns={0:"precio_transformado"})
+    df3 =  df.iloc[13: , :]["fecha"]
+    df3.index = [ i  for i in range(df3.shape[0]) ]
+
+
+    df_completo = df1.join(df2).join(df3)
     df_completo.to_csv(path_salida,index=False,header=True)
+
+    #print(pd.DataFrame(observed_scaled))
+    #print(df.shape)
+
+
 
 
 
@@ -37,6 +55,30 @@ def make_features():
     
     #raise NotImplementedError("Implementar esta función")
 
+
+
+def get_precios(df):
+    return list(df["precio"])
+
+
+def get_precio_transformado(data,P=13):
+    # crea el transformador
+    scaler = MinMaxScaler()
+
+    # escala la serie
+    data_scaled = scaler.fit_transform(np.array(data).reshape(-1, 1))
+
+    # z es un array de listas como efecto
+    # del escalamiento
+    data_scaled = [u[0] for u in data_scaled]
+
+    X = []
+    for t in range(P - 1, len(data) - 1):
+        X.append([data_scaled[t - n] for n in range(P)])
+
+    observed_scaled = data_scaled[P:]
+
+    return (observed_scaled,X)
 
 
 def get_df_variables_explicativas(df,p=13):
--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -1,3 +1,13 @@
+import pandas as pd
+import os
+from sklearn.neural_network import MLPRegressor
+import numpy as np
+
+cwd = os.getcwd()
+
+path_archivo = os.path.join(cwd,"data_lake/business/features/precios_diarios.csv")
+
+
 def train_daily_model():
     """Entrena el modelo de pronóstico de precios diarios.
 
@@ -6,10 +16,36 @@ def train_daily_model():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    data = leer_data()
+    print(data)
+    #raise NotImplementedError("Implementar esta función")
+
+def entrenar(datos):
+    np.random.seed(123456)
+
+    H = 1  # Se escoge arbitrariamente
+
+    mlp = MLPRegressor(
+        hidden_layer_sizes=(H,),
+        activation="logistic",
+        learning_rate="adaptive",
+        momentum=0.0,
+        learning_rate_init=0.1,
+        max_iter=10000,
+    )
+
+    # Entrenamiento
+    mlp.fit(X[0:215], observed_scaled[0:215])  # 239 - 24 = 215
+
+
+def leer_data():
+    datos = pd.read_csv(path_archivo)
+    
+    return list(datos["variables"])[:-13]
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    train_daily_model()
