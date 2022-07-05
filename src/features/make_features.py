87f54892016bfcf664fc46b5c872256a90b464e0
Luis Gabriel Diaz Fonseca <lgdiazf@unal.edu.co>
2022-07-05 03:17:17 +0000

    crear set entrenamiento

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -1,3 +1,11 @@
+import os
+import pandas as pd
+
+cwd = os.getcwd()
+
+path_datos = os.path.join(cwd,"data_lake/business/precios-diarios.csv")
+path_salida = os.path.join(cwd,"data_lake/business/features/precios-diarios.csv")
+
 def make_features():
     """Prepara datos para pronóstico.
 
@@ -12,10 +20,43 @@ def make_features():
     analizar y determinar las variables explicativas del modelo.
 
     """
-    raise NotImplementedError("Implementar esta función")
 
+    df = pd.read_csv(path_datos)
+    df =  df.iloc[13: , :]
+
+    df_variables_explicativas = get_df_variables_explicativas(df,p=13)
+
+    df_completo = df.join(df_variables_explicativas)
+
+    df_completo.to_csv(path_salida)
+
+
+
+
+
+    
+    #raise NotImplementedError("Implementar esta función")
+
+
+
+def get_df_variables_explicativas(df,p=13):
+    df_variables_explicativas = df.apply(lambda row : get_data(row.name,df,p),axis=1)
+    df_variables_explicativas = df_variables_explicativas.to_frame()
+
+    df_variables_explicativas = df_variables_explicativas.rename(columns={0 : "variables"})
+
+    return df_variables_explicativas
+
+
+def get_data(index,df,p):
+
+    inicio = index - p
+    final = inicio + p
+
+    return list(df.iloc[inicio:final]["precio"])
 
 if __name__ == "__main__":
     import doctest
+    make_features()
 
     doctest.testmod()
