f82899559979062a8eba2d2e2369d190837767d6
Luis Gabriel Diaz Fonseca <lgdiazf@unal.edu.co>
2022-07-05 09:40:52 +0000

    arreglo codigo

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -27,19 +27,18 @@ def create_data_lake():
     ```
     Para crear crear las carpetas que componen el datalake se debe ejecutar :
     >>> create_data_lake()
-    True
     """
     for path_carpeta in esquema_data_lake.get_esquema():
         try:
             if not os.path.exists(path_carpeta):
                 path_absoluto = os.path.join(os.getcwd(), path_carpeta)
                 os.mkdir(path_absoluto)
-            return True
-        except  FileNotFoundError  :
-            return False
+        except   :
+            raise Exception("no se pudo crear la carpeta")
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    create_data_lake()
